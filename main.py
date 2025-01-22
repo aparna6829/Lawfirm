762044945a5fdf37c470218358bc639379d64f2b
AishwaryaThimmareddy <aparna6829@gmail.com>
2025-01-22 16:01:36 +0530

    updated main.py

--- main.py
+++ main.py
@@ -175,10 +175,13 @@ def main():
                 processed_response = process_input(user_input, placeholders1, placeholders2,placeholders3,placeholders4,placeholders5,placeholders6,placeholders7)
                 processed_response = processed_response.content
                 fresponse = processed_response.replace('[','{').replace(']','}')
+                print(fresponse)
                 try:
                     fresponse = fresponse.split("```json")[1].split("```")[0]
                     fresponse = json.loads(fresponse)
+                    print("one")
                 except:
+                    print("two")
                     fresponse = json.loads(fresponse)
                 st.session_state.state['document_type'] = fresponse.get("document", "")
                 st.session_state.state['placeholders'] = fresponse.get("placeholders", {})
