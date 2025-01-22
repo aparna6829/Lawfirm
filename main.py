d175f76cac488c4e9b2bd7fc26c1d563102acbf5
BabyAparna <aparna6829@gmail.com>
2025-01-22 17:24:11 +0530

    Updated

--- main.py
+++ main.py
@@ -175,16 +175,16 @@ def main():
                 processed_response = process_input(user_input, placeholders1, placeholders2,placeholders3,placeholders4,placeholders5,placeholders6,placeholders7)
                 processed_response = processed_response.content
                 st.write(processed_response)
-                fresponse = processed_response.replace('[','{').replace(']','}')
+                fresponse = processed_response.replace('[[', '{').replace(']]', '}')
                 st.write(fresponse)
-                # try:
-                #     # fresponse = fresponse.split("```json")[1].split("```")[0]
-                #     fresponse = json.loads(fresponse)
-                #     st.write(fresponse)
-                #     # st.write("one")
-                # except:
-                #     # st.write("two")
-                #     fresponse = json.loads(fresponse)
+                try:
+                    fresponse = fresponse.split("```json")[1].split("```")[0]
+                    fresponse = json.loads(fresponse)
+                    st.write(fresponse)
+                    # st.write("one")
+                except:
+                    # st.write("two")
+                    fresponse = json.loads(fresponse)
                 st.session_state.state['document_type'] = fresponse.get("document", "")
                 st.session_state.state['placeholders'] = fresponse.get("placeholders", {})
                 st.session_state.state['processed'] = True
