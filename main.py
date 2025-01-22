b263b0bc44d81dae55680ae69295685236e88d9b
AishwaryaThimmareddy <aparna6829@gmail.com>
2025-01-22 16:57:34 +0530

    Debugging

--- main.py
+++ main.py
@@ -177,14 +177,14 @@ def main():
                 st.write(processed_response)
                 fresponse = processed_response.replace('[','{').replace(']','}')
                 st.write(fresponse)
-                try:
-                    fresponse = fresponse.split("```json")[1].split("```")[0]
-                    fresponse = json.loads(fresponse)
-                    st.write(fresponse)
-                    # st.write("one")
-                except:
-                    # st.write("two")
-                    fresponse = json.loads(fresponse)
+                # try:
+                #     # fresponse = fresponse.split("```json")[1].split("```")[0]
+                #     fresponse = json.loads(fresponse)
+                #     st.write(fresponse)
+                #     # st.write("one")
+                # except:
+                #     # st.write("two")
+                #     fresponse = json.loads(fresponse)
                 st.session_state.state['document_type'] = fresponse.get("document", "")
                 st.session_state.state['placeholders'] = fresponse.get("placeholders", {})
                 st.session_state.state['processed'] = True
