071fb6c89b908739fab7e7b51a59cd33a6726db8
Aparna <aparna6829@gmail.com>
2025-05-08 09:54:12 +0530

    updated main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from Evidence_2 import query_image
 import requests
 import json
 import io
-from second import process_input, add_content_to_document, doc1_path,doc2_path, doc3_path, doc4_path, doc5_path,doc6_path, doc7_path, placeholders1,placeholders2,placeholders3,placeholders4,placeholders5,placeholders6,placeholders7
+from second import process_input, add_content_to_document, doc1_path,doc2_path,  placeholders1,placeholders2
 # from compliance import run_workflow, compliance_workflow
 # from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext
 
